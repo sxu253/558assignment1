15b063c9e5886673671042c9a2edbf79b87c8b7c
asmitasingla <asmitasingla@localhost>
2020-02-24 16:33:06 -0800

    Merged the common implementation method in to the TCP function.

--- tcs558/src/main/java/Client.java
+++ tcs558/src/main/java/Client.java
@@ -19,7 +19,26 @@ public class Client {
 			OutputStream output = socket.getOutputStream();
 			PrintWriter writer = new PrintWriter(output, true);
 
-			implementTasks(task, key, value, reader, writer);
+			if (task != null) {
+				writer.println(task + " " + key + " " + value);
+
+				if (task.equalsIgnoreCase("get")) {
+					System.out.println(reader.readLine());
+				}
+				if (task.equalsIgnoreCase("store")) {
+					System.out.println(reader.readLine());
+				}
+				if (task.equalsIgnoreCase("exit")) {
+					System.out.println(reader.readLine());
+				}
+				
+			} else {
+				System.out.println("uc/tc <address> <port> put <key> <msg> UDP/TCP CLIENT: Put an object into store\n"
+						+ "uc/tc <address> <port> get <key> UDP/TCP CLIENT: Get an object from store by key\n"
+						+ "uc/tc <address> <port> del <key> UDP/TCP CLIENT: Delete an object from store by key\n"
+						+ "uc/tc <address> <port> store UDP/TCP CLIENT: Display object store\n"
+						+ "uc/tc <address> <port> exit UDP/TCP CLIENT: Shutdown server ");
+			}
 
 		} catch (UnknownHostException ex) {
 
@@ -57,32 +76,5 @@ public class Client {
 			System.out.println("I/O error: " + ex.getMessage());
 		}
 	}
-
-
-	// implement conditions for input tasks
-	private void implementTasks(String task, String key, String value, BufferedReader reader, PrintWriter writer)
-			throws IOException {
-		if (task != null) {
-			writer.println(task + " " + key + " " + value);
-
-			if (task.equalsIgnoreCase("get")) {
-				System.out.println(reader.readLine());
-			}
-			if (task.equalsIgnoreCase("store")) {
-				System.out.println(reader.readLine());
-			}
-			if (task.equalsIgnoreCase("exit")) {
-				System.out.println(reader.readLine());
-			}
-			
-		} else {
-			System.out.println("uc/tc <address> <port> put <key> <msg> UDP/TCP CLIENT: Put an object into store\n"
-					+ "uc/tc <address> <port> get <key> UDP/TCP CLIENT: Get an object from store by key\n"
-					+ "uc/tc <address> <port> del <key> UDP/TCP CLIENT: Delete an object from store by key\n"
-					+ "uc/tc <address> <port> store UDP/TCP CLIENT: Display object store\n"
-					+ "uc/tc <address> <port> exit UDP/TCP CLIENT: Shutdown server ");
-		}
-	}
-	
 	
 }
